3a70573a0f90fb3c97fe426c8412322f16ee5388
Pete475 <90112229+Pete475@users.noreply.github.com>
2025-04-24 18:22:17 -0600

    inital work PS on soloPAge and ProgressBar

---
+++ src/components/OtherComponents/ProgressBar.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+interface ProgressCircleProps {
+  value: number; 
+  label: string;
+  color?: string; 
+}
+
+const ProgressBar: React.FC<ProgressCircleProps> = ({ value, label, color = '#3e98c7' }) => {
+    return (
+        <div style={{ width: 100, margin: 10 }}> 
+          <CircularProgressbar
+            value={value}
+            text={`${value}%`}
+            styles={buildStyles({
+                textColor: '#000',
+                pathColor: color,
+                trailColor: 'eee', 
+            })}
+          /> 
+          <div style={{ textAlign: 'center', marginTop: '8px' }}>{label}</div>
+        </div>
+    );
+};
+
+export default ProgressCircle; 
--- src/components/SoloPage/SoloPage.tsx
+++ src/components/SoloPage/SoloPage.tsx
@@ -1,2 +1,56 @@
 //main solo page component (pull other trackers in here)
 // also include banner (pull in from props) 
+
+import React, { useState } from 'react'; 
+import WorkoutTracker from './WorkoutTracker'; 
+import DietTracker from './DietTracker'; 
+import LangTracker from './LangTracker'; 
+
+interface ProgressState {
+    workout: number; 
+    diet: number, 
+    language: number; 
+}
+
+const UserPage: React.FC = () => {
+    const [progress, setProgress] = useState<ProgressState>({
+        workout: 0,
+        diet: 0,
+        language: 0,
+    }); 
+//replace this withe variables passed in from trip template 
+    const tripGoals {
+        workout: 20, 
+        diet: 10,
+        language: 15, 
+    }; 
+
+    const handleIncrement = (key: keyof ProgressState) => {
+        setProgress(prev => ({
+            ...prev, 
+            [key]: Math.min(prev[key] + 1, tripGoals[key]),
+        }));
+    };
+
+    return (
+        <div style={{ display: flex, justifyContent: 'center', gap: '2rem' }}>
+            <WorkoutTracker
+              value={progress.workout}
+              goal={tripGoals.workout}
+              onIncrement={() => handleIncremenet('workout')}
+            />
+            <DietTracker
+              value={progress.diet}
+              goal={tripGoals.diet}
+              onIncrement={() => handleIncremenet('diet')}
+            />
+            <LangTracker
+              value={progress.language}
+              goal={tripGoals.language}
+              onIncrement={() => handleIncremenet('language')}
+            />
+        </div>
+    );
+};
+
+export default SoloPage; 
