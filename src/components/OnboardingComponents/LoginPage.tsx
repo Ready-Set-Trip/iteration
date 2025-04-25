0bc1de9e88892263d90b510319ebbfc9f7825a6e
Sandar <chocolate.fully@gmail.com>
2025-04-24 17:22:45 -0700

    loginSignup_workingprogress

--- src/components/OnboardingComponents/LoginPage.tsx
+++ src/components/OnboardingComponents/LoginPage.tsx
@@ -1,2 +1,49 @@
-//login page here. 
-//signup button if no login info yet 
+//login page here.
+//signup button if no login info yet
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './LoginPage.css';
+import email_icon from '../../assets/email.png';
+import password_icon from '../../assets/password.png';
+const LoginPage = () => {
+  return (
+    <div className='page-wrapper'>
+      <div className='logcontainer'>
+        <form action=''>
+          <div className='logheader'>
+            <div className='text'>Log In</div>
+            <div className='underline'></div>
+          </div>
+
+          <div className='loginput-field'>
+            <div className='input'>
+              <img src={email_icon} alt='emailIcon' />
+              <input type='email' name='email' id='email' placeholder='Email' />
+            </div>
+            <div className='input'>
+              <img src={password_icon} alt='passwordIcon' />
+              <input
+                type='password'
+                name='password'
+                id='password'
+                placeholder='Password'
+              />
+            </div>
+          </div>
+          <div className='forgot-password'>
+            Forgot Password?<span>Click Here</span>
+          </div>
+          <div className='submit-container'>
+            <button className='submit'>Log In</button>
+            <Link to='/signup' className='submit'>
+              Sign Up
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
--- src/components/OnboardingComponents/SignUp.tsx
+++ src/components/OnboardingComponents/SignUp.tsx
@@ -1,2 +1,52 @@
-// user signup! 
-// link back to login page after signup 
+// user signup!
+// link back to login page after signup
+import React from 'react';
+import './SignUp.css';
+import { Link } from 'react-router-dom';
+import user_icon from '../../assets/person.png';
+import email_icon from '../../assets/email.png';
+import password_icon from '../../assets/password.png';
+
+const SignUp = () => {
+  return (
+    <div className='page-wrapper'>
+      <div className='container'>
+        <form action=''>
+          <div className='header'>
+            <div className='text'>Sign Up</div>
+            <div className='underline'></div>
+          </div>
+
+          <div className='input-field'>
+            <div className='input'>
+              <img src={user_icon} alt='userIcon' />
+              <input type='text' name='name' id='name' placeholder='Name' />
+            </div>
+            <div className='input'>
+              <img src={email_icon} alt='emailIcon' />
+              <input type='email' name='email' id='email' placeholder='Email' />
+            </div>
+            <div className='input'>
+              <img src={password_icon} alt='passwordIcon' />
+              <input
+                type='password'
+                name='password'
+                id='password'
+                placeholder='Password'
+              />
+            </div>
+          </div>
+
+          <div className='submit-container'>
+            <div className='submit'>Sign Up</div>
+            <Link to='/' className='submit'>
+              Log In
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
