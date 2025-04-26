b63baf98d6064a846289e5ffa0751f4666902f33
Pete475 <90112229+Pete475@users.noreply.github.com>
2025-04-26 14:02:58 -0600

    messageboard working

--- src/components/GroupTripPage/MessageBoard.tsx
+++ src/components/GroupTripPage/MessageBoard.tsx
@@ -2,45 +2,80 @@
 
 import React, { useState } from 'react';
 
-function MessageBoard() {
-  const [messages, setMessages] = useState<string[]>([]);
-  const [newMessage, setNewMessage] = useState(''); 
+interface Message {
+  id: number;
+  author: string;
+  text: string;
+}
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNewMessage(e.target.value);   
+const MessageBoard: React.FC = () => {
+  const [messages, setMessages] = useState<Message[]>([
+    { id: 0, author: 'TripLeader', text: 'Here we gooo!' },
+  ]);
+  const [newMessage, setNewMessage] = useState('');
+  const [newAuthor, setNewAuthor] = useState('');
+
+  const handleInputChangeAuthor = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewAuthor(e.target.value);
+  };
+
+  const handleInputChangeText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewMessage(e.target.value);
   };
 
   const handlePostMessage = () => {
-    if (newMessage.trim() !== '') {
-        setMessages([...messages, newMessage]);
-        setNewMessage(''); 
+    if (newMessage.trim() !== '' && newAuthor.trim() !== '') {
+      const newMessageToAdd: Message = {
+        id: messages.length + 1,
+        author: newAuthor,
+        text: newMessage,
+      };
+      setMessages([newMessageToAdd, ...messages]);
+      setNewMessage('');
+      setNewAuthor('');
     }
-  }; 
+  };
 
   return (
     <div>
       <h4>Get ready for your trip!</h4>
       <div>
+        <div
+          style={{
+            overflow: 'scroll',
+            height: '120px',
+            border: '1px solid #fff',
+            padding: '5px',
+          }}
+        >
+          {messages.map((message) => (
+            <div key={message.id}>
+              <p>
+                <strong>{message.author}</strong>: {message.text}
+              </p>
+            </div>
+          ))}
+        </div>
         <input
           type='text'
-          value={newMessage}
-          onChange={handleInputChange}
-          placeholder='Whaddya wanna say to your TripMates?!'
+          placeholder='Your Name'
+          value={newAuthor}
+          onChange={handleInputChangeAuthor}
+          style={{ width: '300px' }}
         />
-        {/* <textarea
+        <div style={{ height: '5px' }} />
+        <textarea
           rows={3}
-          placeholder="Message Here"
+          placeholder='Message Here'
           value={newMessage}
-          onChange={handleInputChange} */}
+          onChange={handleInputChangeText}ç
+          style={{ width: '300px' }}
+        />
+        <br />
         <button onClick={handlePostMessage}>Post</button>
       </div>
-      <ul>
-        {messages.map((message, index) => (
-          <li key={index}>{message}</li>
-        ))}
-      </ul>
     </div>
   );
-}
+};
 
 export default MessageBoard;
