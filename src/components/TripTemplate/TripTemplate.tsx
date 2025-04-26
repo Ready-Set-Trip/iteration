d483d49d99c303e0acc897ea08f0fd69d4ba76be
Sandar <chocolate.fully@gmail.com>
2025-04-26 10:46:10 -0700

    triptemplate

--- src/components/TripTemplate/TripTemplate.tsx
+++ src/components/TripTemplate/TripTemplate.tsx
@@ -1,3 +1,25 @@
 // manages the trip template components.
 // after all filled in, submit button will create your trip id
 // and send it to your friends (via email link) to come to the site and set up their profiles
+import React from 'react';
+import './template.css';
+const TripTemplate = () => {
+  return (
+    <div className='nametripcontainer'>
+      <div className='tripname'>
+        <label>Name Your Trip</label>
+        <input type='text' placeholder='Enter your trip name'></input>
+      </div>
+      <div className='invitecontainer'>
+        <label> Invite Your Friends</label>
+        <input type='email' placeholder='Enter email'></input>
+        <input type='email' placeholder='Enter email'></input>
+        <input type='email' placeholder='Enter email'></input>
+        <input type='email' placeholder='Enter email'></input>
+        <input type='email' placeholder='Enter email'></input>
+      </div>
+    </div>
+  );
+};
+
+export default TripTemplate;
